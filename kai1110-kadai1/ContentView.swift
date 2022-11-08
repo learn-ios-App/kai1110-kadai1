f7569aafe90d024305dc88d374a1ea8a2eb5cb11
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-11-08 21:37:14 +0900

    フィードバックを確認

--- kai1110-kadai1/ContentView.swift
+++ kai1110-kadai1/ContentView.swift
@@ -1,9 +1,3 @@
-//
-//  ContentView.swift
-//  kai1110-kadai1
-//
-//  Created by 渡邊魁優 on 2022/11/06.
-//
 
 import SwiftUI
 
@@ -16,42 +10,48 @@ struct ContentView: View {
     @State private var sumNumber = 0
     
     var body: some View {
-        VStack {
+        VStack(spacing: 32) {
             TextField("", text:$number1)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
             TextField("", text:$number2)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
             TextField("", text:$number3)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
             TextField("", text:$number4)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
             TextField("", text:$number5)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
+
             
             Button(action: {
-                sumNumber = 0
-                plusNumber(number: number1)
-                plusNumber(number: number2)
-                plusNumber(number: number3)
-                plusNumber(number: number4)
-                plusNumber(number: number5)
+                let num1 = parseNumber(number: number1)
+                let num2 = parseNumber(number: number2)
+                let num3 = parseNumber(number: number3)
+                let num4 = parseNumber(number: number4)
+                let num5 = parseNumber(number: number5)
+                sumNumber = num1 + num2 * num3 + num4 + num5
+                
+//                sumNumber = 0
+//                plusNumber(number: number1)
+//                plusNumber(number: number2)
+//                plusNumber(number: number3)
+//                plusNumber(number: number4)
+//                plusNumber(number: number5)
             }) {
                 Text("計算")
             }
-            .padding()
             Text("合計:\(sumNumber)")
         }
+        //textFieldstyleはまとめることができる。
+        .keyboardType(.numberPad)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .padding()
     }
-    func plusNumber(number: String) {
-        if let unwrappedNumber = Int(number) {
-            sumNumber += unwrappedNumber
-        }
+    
+    func parseNumber(number: String) -> Int {
+        Int(number) ?? 0
     }
+    
+//    func plusNumber(number: String) {
+//        if let unwrappedNumber = Int(number) {
+//            sumNumber += unwrappedNumber
+//        }
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
